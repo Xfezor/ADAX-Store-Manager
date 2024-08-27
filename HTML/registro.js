b84297c6f93d30a1706a94980219496585501efc
Xfezor <damono600d@gmail.com>
2024-08-27 07:36:06 -0500

    arreglo colores completado, js de registro completado

--- HTML/registro.js
+++ HTML/registro.js
@@ -1,48 +1,31 @@
+function cambiarestadoBtn1(){
 
-document.querySelector('#empleadoBtn').addEventListener('click', () => {
   setTimeout(function() {
-    document.querySelector('#empleadoBtn').classList.remove('btn1');
-    document.querySelector('#empleadoBtn').classList.add('btn1-active');
-    document.querySelector('#tiendaBtn').classList.remove('btn2-active');
-    document.querySelector('#tiendaBtn').classList.add('btn2');
-    document.querySelector('#cont1').style.display = "none";
     document.querySelector('#cont2').style.display = "block";
-  }, 0);
-
-}); 
-
-document.querySelector('#tiendaBtn').addEventListener('click', () => {
-
-  setTimeout(function() {
-    document.querySelector('#empleadoBtn').classList.remove('btn1-active');
-    document.querySelector('#empleadoBtn').classList.add('btn1');
-    document.querySelector('#tiendaBtn').classList.remove('btn2');
-    document.querySelector('#tiendaBtn').classList.add('btn2-active');
     document.querySelector('#cont1').style.display = "none";
-    document.querySelector('#cont2').style.display = "block";
-  }, 0);
-}); 
+  }, 1);
 
-document.querySelector('#empleadoBtn1').addEventListener('click', () => {
+};
 
+function cambiarestadoBtn2(){
   setTimeout(function() {
-    document.querySelector('#empleadoBtn1').classList.remove('btn1');
-    document.querySelector('#empleadoBtn1').classList.add('btn1-active');
-    document.querySelector('#tiendaBtn1').classList.remove('btn2-active');
-    document.querySelector('#tiendaBtn1').classList.add('btn2');
-    document.querySelector('#cont2').style.display = "none";
+    document.querySelector('#cont2').style.display = "block";
+    document.querySelector('#cont1').style.display = "none";
+  }, 1);
+};
+
+function cambiarestadoBtn11(){
+    setTimeout(function() {
     document.querySelector('#cont1').style.display = "block";
-  }, 0);
-}); 
+    document.querySelector('#cont2').style.display = "none";
+  }, 1);
+};
 
-document.querySelector('#tiendaBtn1').addEventListener('click', () => {
+function cambiarestadoBtn22(){
 
   setTimeout(function() {
-    document.querySelector('#empleadoBtn1').classList.remove('btn1-active');
-    document.querySelector('#empleadoBtn1').classList.add('btn1');
-    document.querySelector('#tiendaBtn1').classList.remove('btn2');
-    document.querySelector('#tiendaBtn1').classList.add('btn2-active');
-    document.querySelector('#cont1').style.display = "none";
     document.querySelector('#cont2').style.display = "block";
-  }, 0);
-}); 
+    document.querySelector('#cont1').style.display = "none";
+
+  }, 1);
+};
