090a96f163ac1ee005b0d61e1c2dba1fd61311a7
Xfezor <damono600d@gmail.com>
2024-11-24 10:22:40 -0500

    regreso iniciar_sesion

--- adax/src/App.js
+++ adax/src/App.js
@@ -1,12 +1,13 @@
 
 import Index from './pages/index.jsx';
-//import IniciarSesion from './pages/iniciar_sesion.jsx';
+import IniciarSesion from './pages/iniciar_sesion.jsx';
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Index />}></Route>
+        <Route path='/iniciar_sesion' element={<IniciarSesion />}></Route>
         <Route path='*' element={<Index />}></Route>
       </Routes>
     </div>
