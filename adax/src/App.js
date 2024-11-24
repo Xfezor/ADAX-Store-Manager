a2577f7c56dca75466cc0eb945e041d7c87f783c
Santyx2006 <santiago.martinez72@misena.edu.co>
2024-11-24 14:22:45 -0500

    gestionar_productos falta mejorar los estilos

--- adax/src/App.js
+++ adax/src/App.js
@@ -1,16 +1,18 @@
 import Index from './pages/index.jsx';
 import IniciarSesion from './pages/iniciar_sesion.jsx';
 import Analisis from './pages/analisis.jsx';
-import { Routes, Route } from "react-router-dom";
+import GestionarProductos from './pages/gestionar_productos.jsx';
+import { Routes, Route } from "react-router-dom";   
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Index />}></Route>
-        <Route path='/iniciar_sesion' element={<IniciarSesion />}></Route>
-        <Route path='/analisis' element={<Analisis />}></Route>
-        <Route path='*' element={<Index />}></Route>
+        <Route path="/" element={<Index />} />
+        <Route path="/iniciar_sesion" element={<IniciarSesion />} />
+        <Route path="/analisis" element={<Analisis />} />
+        <Route path="/gestionar_productos" element={<GestionarProductos />} />
+        <Route path="*" element={<Index />} />
       </Routes>
     </div>
   );
