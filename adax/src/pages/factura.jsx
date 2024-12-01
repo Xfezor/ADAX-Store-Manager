9ad62d08e14ad045e2981e9dec0e085e9366465b
Xfezor <damono600d@gmail.com>
2024-12-01 15:07:28 -0500

    factura y generar pago 100%

--- adax/src/pages/factura.jsx
+++ adax/src/pages/factura.jsx
@@ -1,71 +1,81 @@
 import React from "react";
-import '../styles/styles_factura.module.css';
+import styles from '../styles/styles_factura.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
-
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 function Factura() {
-    const navigate = useNavigate(); 
-    const backButtonHandler = () => {
-        console.log("Volver");
+    const navigate = useNavigate();
+
+
+    const backbutton = () => {
+        console.log("Volver atrás");
+
         navigate(-1);
-       
     };
 
-    const exitButtonHandler = () => {
-       
+    const exitbutton = () => {
         console.log("Salir");
+
     };
 
     return (
         <>
             <header>
-                <div className="contenedorarriba">
-                    <button className="back" onClick={backButtonHandler}>
-                        <Link to="/factura" className="fa-solid fa-arrow-left"></Link>
+                <div className={styles.contenedorarriba}>
+                    <button className={styles.back} onClick={backbutton}>
+                        <FontAwesomeIcon icon={faArrowLeft} />
                     </button>
-                    <div className="adax">
-                        <h1 className="title">Factura</h1>
+                    <div className={styles.adax}>
+                        <h1 className={styles.title}>Factura</h1>
                     </div>
-                    <button className="exit" onClick={exitButtonHandler}>
-                        <i className="fa-solid fa-xmark"></i>
+                    <button className={styles.exit} onClick={exitbutton}>
+                        <FontAwesomeIcon icon={faXmark} clasName={styles.exit} />
                     </button>
                 </div>
             </header>
 
-            <div className="page">
-                <div className="left-container">
-                    <h1 className="big-text">Dinero recibido</h1>
-                    <div className="cash-list">
-                        <h2 className="cantidad-text">Cantidad recibida: </h2>
-                        <h3 className="total-recived"> =$50.000</h3>
-                        <h2 className="mediodepago-text">Medio de pago: </h2>
-                        <h2 className="mediodepago-elejido"> Efectivo </h2>
-                        <h2 className="estado-text">Estado: </h2>
-                        <h2 className="estado"> = Ok </h2>
-                        <h3 className="total-text">Total pagado:</h3>
-                        <h3 className="total-cant-text"> =$32.000</h3>
-                        <h3 className="devolver-text">Devuelto:</h3>
-                        <h3 className="devolver-cant-text"> =$18.000</h3>
+            <div className={styles.page}>
+                <div className={styles['left-container']}>
+                    <h1 className={styles['big-text']}>Dinero recibido</h1>
+                    <div className={styles['cash-list']}>
+                        <h2 className={styles['cantidad-text']}>Cantidad recibida: </h2>
+                        <h3 className={styles['total-recived']}> =$50.000</h3>
+                        <h2 className={styles['mediodepago-text']}>Medio de pago: </h2>
+                        <h2 className={styles['mediodepago-elegido']}> Efectivo </h2>
+                        <h2 className={styles['estado-text']}>Estado: </h2>
+                        <h2 className={styles.estado}> = Ok </h2>
+                        <h3 className={styles['total-text']}>Total pagado:</h3>
+                        <h3 className={styles['total-cant-text']}> =$32.000</h3>
+                        <h3 className={styles['devolver-text']}>Devuelto:</h3>
+                        <h3 className={styles['devolver-cant-text']}> =$18.000</h3>
                     </div>
-                    <button className="generar-pago">Salir</button>
+                    <button className={styles['generar-pago']}>Salir</button>
                 </div>
 
-                <div className="right-container">
-                    <h1 className="big-text">Carrito</h1>
-                    <div className="cart-list">
-                        <h2 className="text-inside"></h2>
+                <div className={styles['right-container']}>
+                    <h1 className={styles['big-text']}>Carrito</h1>
+                    <div className={styles['cart-list']}>
+                        <h2 className={styles['text-inside']}>test</h2>
                     </div>
                 </div>
             </div>
 
             <footer>
-                <div className="user">
-                    <h1 className="username">Usuario: "Pepito Peréz"</h1>
-                    <h1 className="username">Tienda: "Los peregrinos"</h1>
-                    <h1 className="username">Codigo invitacion: "TX435SX"</h1>
-                    <button className="btn btn-danger" id="cerrarsesion">Cerrar sesión</button>
+                <div className={styles.user}>
+                    <h1 className={styles.username}>Usuario: "Pepito Peréz"</h1>
+                    <h1 className={styles.username}>Tienda: "Los peregrinos"</h1>
+                    <h1 className={styles.username}>Codigo invitacion: "TX435SX"</h1>
+                    <a href="../Crud/tablas/tablas.php" className={`btn btn-danger`} id={styles.cerrarsesion}>CRUD
+                    </a>
+                    <button
+                        className="btn btn-danger"
+                        id={styles.cerrarsesion}
+                        onClick=""
+                    >
+                        Cerrar sesión
+                    </button>
                 </div>
             </footer>
         </>
