86929eea9b359844e9fd76d86d2476b2bac9920a
Santyx2006 <martinotes95@gmail.com>
2024-12-02 20:08:02 -0500

    mi intento de gestionar productos

--- adax/src/pages/gestionar_productos.jsx
+++ adax/src/pages/gestionar_productos.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; 
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useNavigate } from 'react-router-dom';
 import styles from '../styles/styles_gestionar_productos.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
@@ -36,82 +37,65 @@ const GestionarProductos = () => {
           </button>
         </div>
       </header>
-      <div className={styles["left-container"]}>
-        <h1 className={styles["big-text"]}>Producto</h1>
-        <input
-          className={styles["search"]}
-          type="text"
-          placeholder="Escriba el nombre de un producto"
-        />
-        <button className="btn btn-danger" id={styles["search-button"]} >
-          Buscar
-        </button>
-        <div className={styles["product-list"]}>
-          <table className={styles["product-table"]}>
-            <thead className={styles["table-head"]}>
-              <tr>
-                <th>Nombre</th>
-                <th>Marca</th>
-                <th>Detalle</th>
-              </tr>
-            </thead>
-            <tbody className="table-body">
-                <tr className={styles.trgespro}>
-                  <td className={styles.tdgespro}></td>
-                  <td className={styles.tdgespro}></td>
-                  <td className={styles.tdgespro}>
-                    <button className={`{btn btn-danger ${styles["search-button"]}`}>
-                      Ver detalle
-                    </button>
-                  </td>
+      <div className={styles.page}>
+        <div className={styles['left-container']}>
+          <h1 className={styles['big-text']}>Producto</h1>
+          <input className={styles.search} type="text" name="search" id="search"
+            placeholder="Escriba un el nombre de un producto"></input>
+          <button className="btn btn-danger" id={styles['search-button']}>Buscar</button>
+          <div className={styles['product-list']}>
+            <table id="productos" className={styles['product-table']}>
+              <thead className={styles['table-head']}>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Marca</th>
+                  <th>detalle</th>
                 </tr>
-            </tbody>
-          </table>
+              </thead>
+              <tbody className={styles['table-body']}>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td><button className="btn btn-danger" id={styles.detalle} onclick="verdetalle()">Ver detalle</button></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
-
-      <div className="right-container">
-        <h1 className="big-text">Añadir producto</h1>
-        <div className="add-product">
-          <form >
-            <input
-              className="product-name"
-              type="text"
-              placeholder="Escriba el nombre del producto"
-            />
-            <input
-              className="product-price"
-              type="text"
-              placeholder="Escriba el precio sin puntos ni comas"
-            />
-            <span style={{ display: 'flex', gap: '10px' }}>
-              <h3 className="text-left">Cantidad:</h3>
-              <input
-                type="number"
-                className="numero"
-                placeholder="Número"
-              />
-            </span>
-            <button className="añadir-producto" type="submit">
-              Añadir producto
-            </button>
-          </form>
-        </div>
-
-        <div className="proveedores">
-          <h1 className="big-text-proveedores">Proveedores</h1>
-          <button className="gestionar-proveedores">
-            Gestionar proveedores
-          </button>
+        <div className={styles['right-container']}>
+          <h1 className={styles['big-text']}>Añadir producto</h1>
+          <div className={styles['add-product']}>
+            <form action="../Crud/controlador/controlador.producto.php" className={styles.form} method="POST">
+              <input className={styles['product-name']} type="text" name="Nombre" id="addproduct"
+                placeholder="Escriba el nombre del producto"></input>
+              <input className={styles['product-price']} type="text" name="Precio_unit" id="productPrice"
+                placeholder="Escriba el precio sin puntos ni comas"></input>
+              <span style={{'display': 'flex', 'gap': '10px'}}>
+                <h3 className={styles['text-left']}>Cantidad: </h3>
+                <input type="number" className={styles.numero} placeholder="Número" name="Stock"  />
+              </span>
+              <button className={styles['añadir-producto']} type="submit" name="registrarProductoUnico" value="1">Añadir producto</button>
+            </form>
+          </div>
+          <div className={styles.proveedores}>
+            <h1 className={styles['big-text-proveedores']}>Proveedores</h1>
+            <button className={styles['gestionar-proveedores']} onclick="gestionarproveedores()">Gestionar proveedores</button>
+          </div>
         </div>
       </div>
 
       <footer>
-        <div className="user">
-          <h1 className="username">Usuario: ""</h1>
-          <h1 className="username">Tienda: ""</h1>
-          <h1 className="username">Código invitación: ""</h1>
-          <button className="btn btn-danger" >
+        <div className={styles.user}>
+          <h1 className={styles.username}>Usuario: "Pepito Peréz"</h1>
+          <h1 className={styles.username}>Tienda: "Los peregrinos"</h1>
+          <h1 className={styles.username}>Codigo invitacion: "TX435SX"</h1>
+          <a href="../Crud/tablas/tablas.php" className={`btn btn-danger`} id={styles.cerrarsesion}>CRUD
+          </a>
+          <button
+            className="btn btn-danger"
+            id={styles.cerrarsesion}
+            onClick=""
+          >
             Cerrar sesión
           </button>
         </div>
