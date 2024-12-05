943df10b3babc8c530f20ca0dec4efdd32ef66b7
Xfezor <damono600d@gmail.com>
2024-12-05 11:45:13 -0500

    compilacion sin errores o advertencias

--- adax/src/pages/restablecercontrasena.jsx
+++ adax/src/pages/restablecercontrasena.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from '../styles/styles_olvide_Contrasena.module.css'; 
+import styles from '../styles/styles_olvide_Contrasena.module.css';
 import '@fontsource-variable/montserrat';
-import { Link } from "react-router-dom";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faArrowLeft} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router-dom';
-import {faXmark} from '@fortawesome/free-solid-svg-icons';
-import Swal from 'sweetalert2';
-
-
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 function sendcodeandhide() {
     if (document.getElementById("input1").value.trim() === "") {
@@ -68,53 +64,49 @@ const RestablecerContrasena = () => {
         console.log("Volver atrás");
         navigate(-1);
     };
-  
+
     const exitbutton = () => {
         console.log("Salir");
         navigate('/inicio');
     };
 
-    
+
     return (
         <>
-            <header className='container-fluid'>
+            <header>
                 <div className={styles.contenedorarriba}>
-                    <button className={styles.back}>
-                        <Link to="/iniciar_sesion">
-                        <FontAwesomeIcon icon={faArrowLeft} className={styles.back}/>
-                        </Link>
+                    <button className={styles.back} onClick={backbutton}>
+                        <FontAwesomeIcon icon={faArrowLeft} />
                     </button>
-                    <div className={styles.adax} >
+                    <div className={styles.adax}>
                         <h1 className={styles.title}>ADAX Store Manager</h1>
                     </div>
-                    <button className={styles.exit}>
-                        <Link to="/index">
-                        <FontAwesomeIcon icon={faXmark}   className={styles.exit}/>
-                        </Link>
+                    <button className={styles.exit} onClick={exitbutton}>
+                        <FontAwesomeIcon icon={faXmark} clasName={styles.exit} />
                     </button>
                 </div>
             </header>
-            <div className={styles.cuadradoverde} style={{display: 'block'}  }>
+            <div className={styles.cuadradoverde} style={{ display: 'block' }}>
                 <form className={styles.Contenedorsesion}>
                     <h1 id="bigtitle1" className={styles.tituloOlvideMiContraseña}><b>Olvidé mi contraseña</b></h1>
-                    <h1 id="bigtitle2" className={styles.tituloOlvideMiContraseña} style={{display: 'none'}}    ><b>Nueva contraseña</b></h1>
+                    <h1 id="bigtitle2" className={styles.tituloOlvideMiContraseña} style={{ display: 'none' }}    ><b>Nueva contraseña</b></h1>
                     <h2 id="text1" className={styles.textoOlvide}><b>Escriba el correo asociado con su cuenta, así le podremos enviar un codigo de confirmación para poder cambiar la contraseña.</b></h2>
-                    <h2 id="text2" className={styles.textoOlvide} style={{display: 'none'}}><b>Se ha enviado un codigo de seguridad al (correo)</b></h2>
-                    <h2 id="text3" className={styles.textoOlvide} style={{display: 'none'}}><b>Escriba su nueva contraseña</b></h2>
+                    <h2 id="text2" className={styles.textoOlvide} style={{ display: 'none' }}><b>Se ha enviado un codigo de seguridad al (correo)</b></h2>
+                    <h2 id="text3" className={styles.textoOlvide} style={{ display: 'none' }}><b>Escriba su nueva contraseña</b></h2>
                     <h1 id="middletittle1" className={styles.usernameText}><b>Correo electronico</b></h1>
-                    <h1 id="middletittle2" className={styles.usernameText} style={{display: 'none'}}><b>Codigo de seguridad</b></h1>
-                    <h1 id="middletittle3" className={styles.usernameText} style={{display: 'none'}}><b>Contraseña</b></h1>
+                    <h1 id="middletittle2" className={styles.usernameText} style={{ display: 'none' }}><b>Codigo de seguridad</b></h1>
+                    <h1 id="middletittle3" className={styles.usernameText} style={{ display: 'none' }}><b>Contraseña</b></h1>
                     <input id="input1" type="email" placeholder="Ingrese su correo electronico" required />
-                    <input id="input2" type="text" placeholder="Ingrese el codigo de seguridad" required style={{display: 'none'}} />
-                    <h1 id="middletittle3" className={styles.usernameText} style={{display: 'none'}}><b>Contraseña</b></h1>
-                    <input id="input3" type="password" placeholder="Ingrese su contraseña" required style={{display: 'none'}} />
+                    <input id="input2" type="text" placeholder="Ingrese el codigo de seguridad" required style={{ display: 'none' }} />
+                    <h1 id="middletittle3" className={styles.usernameText} style={{ display: 'none' }}><b>Contraseña</b></h1>
+                    <input id="input3" type="password" placeholder="Ingrese su contraseña" required style={{ display: 'none' }} />
                     <h1 id="middletittle4" className={styles.usernameText}><b>Repita su contraseña</b></h1>
-                    <input id="input4" type="password" placeholder="Repita su contraseña" required style={{display: 'none'}} />
+                    <input id="input4" type="password" placeholder="Repita su contraseña" required style={{ display: 'none' }} />
                     <div className="invalid-feedback">Por favor, ingrese su usuario</div>
                     <button className="btn btn-danger" id="buttonSendCode" type="submit" onclick={sendcodeandhide}>Enviar código</button>
-                    <button className="btn btn-danger" id="buttonConfirmCode" type="submit" onclick={confirmcode} style={{display: 'none'}}>Confirmar código</button>
+                    <button className="btn btn-danger" id="buttonConfirmCode" type="submit" onclick={confirmcode} style={{ display: 'none' }}>Confirmar código</button>
                 </form>
-                <button className="btn btn-danger" id="buttonChangePswd" onclick={cambiarContra} style={{display: 'none'}}>Cambiar contraseña</button>
+                <button className="btn btn-danger" id="buttonChangePswd" onclick={cambiarContra} style={{ display: 'none' }}>Cambiar contraseña</button>
             </div>
         </>
     )
