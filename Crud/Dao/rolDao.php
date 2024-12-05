30ef84f052c5eae585254e7c80f408226d2399c3
Santyx2006 <santiago.martinez72@misena.edu.co>
2024-12-05 10:53:47 -0500

    Conexion DB rol, falta confimar funciponamiento

---
+++ Crud/Dao/rolDao.php
@@ -0,0 +1,115 @@
+<?php
+class rolDao
+{
+    public function registrarRol(rolDto $rolDto)
+    {
+        $conn = Conexion::getConexion();
+        $mensaje = '';
+        $id_Rol = $rolDto->getid_Rol();
+        $nombreRol = $rolDto->getnombreRol();
+        $descripcion = $rolDto->getdescripcion();
+
+
+        try {
+            $query = $conn->prepare("INSERT INTO rol(id_Rol, nombreRol, descripcion) VALUES(?,?,?)");
+            $query->bindParam(1, $id_Rol);
+            $query->bindParam(2, $nombreRol);
+            $query->bindParam(3, $descripcion);
+
+            $query->execute();
+            $mensaje = 'Rol registrado con exito';
+        } catch (Exception $ex) {
+            $mensaje = $ex->getMessage();
+        }
+        $conn = null;
+        return $mensaje;
+    }
+
+    public function registroRolCrud(rolDto $rolDto)
+    {
+        $conn = Conexion::getConexion();
+        $mensaje = '';
+        $id_Rol = $rolDto->getid_Rol();
+        $nombreRol = $rolDto->getnombreRol();
+        $descripcion = $rolDto->getdescripcion();
+
+        try {
+            $query = $conn->prepare("INSERT INTO rol(id_Rol, nombreRol, descripcion) VALUES(?,?,?)");
+            $query->bindParam(1, $id_Rol);
+            $query->bindParam(2, $nombreRol);
+            $query->bindParam(3, $descripcion);
+            $query->execute();
+            $mensaje = 'Rol registrado con exito';
+        } catch (Exception $ex) {
+            $mensaje = $ex->getMessage();
+        }
+        $conn = null;
+        return $mensaje;
+    }
+    public function listarTodos()
+    {
+        $conn = Conexion::getConexion();
+        try {
+            $listarRoles = 'SELECT * FROM rol';
+            $query = $conn->prepare($listarRoles);
+            $query->execute();
+            return $query->fetchAll();
+        } catch (Exception $ex) {
+            echo 'Error' . $ex->getMessage();
+        }
+    }
+
+    public function modificarRol(rolDto $rolDto)
+    {
+        $conn = Conexion::getConexion();
+        $mensaje = '';
+        $id_Rol = $rolDto->getid_Rol();
+        $nombreRol = $rolDto->getnombreRol();
+        $descripcion = $rolDto->getdescripcion();
+        try {
+            $query = $conn->prepare("UPDATE rol SET nombreRol = ?, descripcion = ? WHERE id_Rol = ?");
+            $query->bindParam(1, $nombreRol);
+            $query->bindParam(2, $descripcion);
+            $query->bindParam(3, $id_Rol);
+
+            $query->execute();
+            $mensaje = 'Rol modificado con exito';
+        } catch (Exception $ex) {
+            $mensaje = $ex->getMessage();
+        }
+        $conn = null;
+        return $mensaje;
+    }
+
+    public function obtenerRol($id_Rol)
+    {
+        $conn = Conexion::getConexion();
+        $mensaje = '';
+        try {
+            $query = $conn->prepare("SELECT * FROM rol WHERE id_Rol = ?");
+            $query->bindParam(1, $id_Rol);
+            $query->execute();
+            return $query->fetch();
+        } catch (Exception $ex) {
+            $mensaje = $ex->getMessage();
+        }
+        $conn = null;
+        return $mensaje;
+    }
+
+    public function eliminarRol($id_Rol)
+    {
+        $conn = Conexion::getConexion();
+        $mensaje = '';
+        try {
+            $query = $conn->prepare("DELETE FROM rol WHERE id_Rol = ?");
+            $query->bindParam(1, $id_Rol);
+            $query->execute();
+            $mensaje = 'Rol eliminado con exito';
+        } catch (Exception $ex) {
+            $mensaje = $ex->getMessage();
+        }
+        $conn = null;
+        return $mensaje;
+    }
+}
