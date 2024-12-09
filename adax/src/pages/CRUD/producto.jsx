bfc7dc8014b55aa3e90025c2ac2990642fafe976
Xfezor <damono600d@gmail.com>
2024-12-09 16:35:17 -0500

    componentes del crud, com 3 tablas, falta borrar y actualizar

---
+++ adax/src/pages/CRUD/producto.jsx
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import DataTable from 'datatables.net-react';
+import DT from 'datatables.net-dt';
+import axios from 'axios';
+
+
+const Producto = () => {
+    DataTable.use(DT);
+    const [producto, setProducto] = useState([]);
+
+    const Lista = async () => {
+        try {
+            const respuesta = await axios.post(`http://localhost/adx/ADAX-Store-Manager/Crud/controlador/controlador.producto.php`, {
+                listar: true,
+            });
+            console.log(respuesta.data);
+            if (respuesta.data) {
+                setProducto(respuesta.data);
+            } else {
+                console.log('listado no exitoso', respuesta.data)
+                return null;
+            }
+        } catch (err) {
+            console.error(err);
+            return null;
+        }
+        console.log(producto);
+    }
+    useEffect(() => {
+        Lista();
+    }, []);
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body sticky-top" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="../tablas.php">ADAX - CRUD</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#topnavbarNavDropdown"
+                        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item dropdown"><a className="nav-link dropdown-toggle" href="#top" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">Usuarios</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="usuario/listarusuarios.php">lista</a></li>
+                                    <li><a className="dropdown-item" href="usuario/registrar.php">registrar</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown"><a className="nav-link dropdown-toggle" href="#top" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">Tienda</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="tienda/listartienda.php">lista</a></li>
+                                    <li><a className="dropdown-item" href="tienda/registrar.php">registrar</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle active" href="#top" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">Producto</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="producto/listarproducto.php">lista</a></li>
+                                    <li><a className="dropdown-item" href="producto/registrar.php">registrar</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#top" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">Factura</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="factura/listarfactura.php">lista</a></li>
+                                    <li><a className="dropdown-item" href="factura/registrar.php">registrar</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#top" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">Venta</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="venta/listarventa.php">lista</a></li>
+                                    <li><a className="dropdown-item" href="venta/registrar.php">registrar</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#top" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">Proveedor</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="proveedor/listarproveedor.php">lista</a></li>
+                                    <li><a className="dropdown-item" href="proveedor/registrar.php">registrar</a></li>
+                                </ul>
+                            </li>
+
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#top" role="button" data-bs-toggle="dropdown" aria-expanded="false">Movimiento</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="movimiento/listarmovimiento.php">lista</a></li>
+                                    <li><a className="dropdown-item" href="Amovimiento/registrar.php">registrar</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <span className="navbar-text me-3 active">Usuario:
+                        </span>
+                        <a href="cerrarsesion.php" className="btn btn-outline-danger float-right end-0 me-0" type="submit">cerrar
+                            sesión</a>
+                    </div>
+                </div>
+            </nav>
+            <div style={{ 'width': '99.9%' }}>
+                <DataTable data={producto} slots={{
+                    11: (data, row) => (
+                        <form action="actualizar.php" method="post">
+                            <input type="hidden" name="doc" value={producto[0]} />
+                            <button type="submit" className="btn btn-warning">Modificar</button>
+                        </form>
+                    ),
+                    12: (data, row) => (
+                        <a className="btn btn-danger" href={`../../controlador/controlador.usuarios.php?docu=${producto[0]}`}>
+                            Eliminar
+                        </a>
+                    )
+                }} id="usrtable" className="table table-container table-striped table-hover table-bordered table-responsive mt-4 table-sm">
+                    <thead className="table-dark light-header">
+                        <tr className="text-center">
+                            <th style={{ 'fontWeight': 'normal' }}>id_Producto</th>
+                            <th style={{ 'fontWeight': 'normal' }}>Nombre</th>
+                            <th style={{ 'fontWeight': 'normal' }}>Precio_unit</th>
+                            <th style={{ 'fontWeight': 'normal' }}>Descripcion</th>
+                            <th style={{ 'fontWeight': 'normal' }}>Marca</th>
+                            <th style={{ 'fontWeight': 'normal' }}>Categoria</th>
+                            <th style={{ 'fontWeight': 'normal' }}>Presentacion</th>
+                            <th style={{ 'fontWeight': 'normal' }}>Fecha_vencimiento</th>
+                            <th style={{ 'fontWeight': 'normal' }}>Stock</th>
+                            <th style={{ 'fontWeight': 'normal' }}>Stock_Min</th>
+                            <th style={{ 'fontWeight': 'normal' }}>inventario_id_Inventario</th>
+                            <th style={{ 'fontWeight': 'normal' }}>Modificar</th>
+                            <th style={{ 'fontWeight': 'normal' }}>Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </DataTable>
+            </div>
+        </div>
+    );
+}
+
+export default Producto;
+
