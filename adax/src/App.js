bb98ac3c4d6abefb2fdfe5a611c93dd298928c36
Xfezor <damono600d@gmail.com>
2024-12-10 13:07:45 -0500

    intento de usar contexto para actualizar fallido

--- adax/src/App.js
+++ adax/src/App.js
@@ -1,13 +1,15 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
 import Index from './pages/index.jsx';
 import IniciarSesion from './pages/iniciar_sesion.jsx';
 import Registro from './pages/registro.jsx';
 import Restablecer from './pages/restablecercontrasena.jsx';
-import Factura from './pages/factura.jsx'; 
-import Inicio from './pages/inicio.jsx'; 
-import Pago from './pages/generar_pago.jsx'; 
-import Ventas from './pages/ventas.jsx'; 
+import Factura from './pages/factura.jsx';
+import Inicio from './pages/inicio.jsx';
+import Pago from './pages/generar_pago.jsx';
+import Ventas from './pages/ventas.jsx';
 import GestionarProductos from './pages/gestionar_productos.jsx';
-import Gestionarproveedores from './pages/gestionar_proveedores.jsx'
+import Gestionarproveedores from './pages/gestionar_proveedores.jsx';
 import Analisis from './pages/analisis.jsx';
 import GestionarVentas from './pages/gestionar_ventas.jsx';
 import DetalleFactura from './pages/detalle_factura.jsx';
@@ -15,31 +17,31 @@ import Usuarios from './pages/CRUD/usuarios.jsx';
 import ActualizarUsuarios from './pages/CRUD/actualizar/actualizarUsuario.jsx';
 import Tienda from './pages/CRUD/tienda.jsx';
 import Producto from './pages/CRUD/producto.jsx';
-import { Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/iniciar_sesion" element={<IniciarSesion />} />
-        <Route path="/registro" element={<Registro />} />
-        <Route path="/restablecer_contrasena" element={<Restablecer />} />
-        <Route path="/generar_pago" element={<Pago />} /> 
-        <Route path="/factura" element={<Factura />} />
-        <Route path="/ventas" element={<Ventas />} />
-        <Route path="/gestionar_productos" element={< GestionarProductos />} />
-        <Route path="/gestionar_proveedores" element={<Gestionarproveedores />}/>
-        <Route path="/inicio" element={<Inicio />} />
-        <Route path="/analisis" element={<Analisis />} />
-        <Route path="/gestionar_ventas" element={<GestionarVentas />} />
-        <Route path="/detalle_factura" element={<DetalleFactura />} />
-        <Route path="/crud/usuarios" element={<Usuarios />} />
-        <Route path="/crud/actualizar/Actualizar_usuario" element={<ActualizarUsuarios />} />
-        <Route path="/crud/tienda" element={<Tienda />} />
-        <Route path="/crud/producto" element={<Producto />} />
-        <Route path="*" element={<Index />} />
-      </Routes>
-    </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/iniciar_sesion" element={<IniciarSesion />} />
+          <Route path="/registro" element={<Registro />} />
+          <Route path="/restablecer_contrasena" element={<Restablecer />} />
+          <Route path="/generar_pago" element={<Pago />} />
+          <Route path="/factura" element={<Factura />} />
+          <Route path="/ventas" element={<Ventas />} />
+          <Route path="/gestionar_productos" element={< GestionarProductos />} />
+          <Route path="/gestionar_proveedores" element={<Gestionarproveedores />} />
+          <Route path="/inicio" element={<Inicio />} />
+          <Route path="/analisis" element={<Analisis />} />
+          <Route path="/gestionar_ventas" element={<GestionarVentas />} />
+          <Route path="/detalle_factura" element={<DetalleFactura />} />
+          <Route path="/crud/usuarios" element={<Usuarios />} />
+          <Route path="/crud/actualizar/actualizar_usuario" element={<ActualizarUsuarios />} />
+
+          <Route path="/crud/tienda" element={<Tienda />} />
+          <Route path="/crud/producto" element={<Producto />} />
+          <Route path="*" element={<Index />} />
+        </Routes>
+      </div>
   );
 }
 
