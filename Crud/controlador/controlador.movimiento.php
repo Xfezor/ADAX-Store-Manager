8033660854ec6f93021ac4af891e1640763fa6f3
LEFINITO <yeyersyt@gmail.com>
2024-12-10 21:18:26 -0500

    cambios en el crud

--- Crud/controlador/controlador.movimiento.php
+++ Crud/controlador/controlador.movimiento.php
@@ -1,28 +1,70 @@
 <?php
+    header("Access-Control-Allow-Origin: *"); // Permite todas las solicitudes de cualquier origen
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Métodos permitidos
+    header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Cabeceras permitidas
+    header('Content-Type: application/json');
+    
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        // Si es una solicitud OPTIONS, simplemente devuelve un 200 OK
+        http_response_code(200);
+        exit();
+    }
+    
+
+
     require '../Dao/movimientoDao.php';
     require '../Dto/movimientoDto.php';
     require '../utilidades/conexion.php';
 
+    $data = json_decode(file_get_contents('php://input'), true);
+    if (isset($data['regristro'])) {
+        $nombreTienda = $data['cantidad_despues'];
+        $telefono = $data['fecha_movimiento'];
+        $email = $data['email'];
+        $contrasena = $data['contrasena'];
+        $direccion = $data['direccion'];
+    }
+    if (isset($data['listar'])) {
+        $listar = $data['listar'];
+    }
+    
+
+
 if (isset($_POST['registro'])){
     $mDao = new MovimientoDao();
     $mDto = new MovimientoDto();
-    $mDto->setId_Movimiento($_POST['id_Movimiento']);
-    $mDto->setCantidad_despues($_POST['cantidad_despues']);
-    $mDto->setFecha_movimiento($_POST['fecha_movimiento']);
-    $mDto->setFecha_modificacion($_POST['fecha_modificacion']);
-    $mDto->setEstado_despues($_POST['estado_despues']);
-    $mDto->setInventario_id_Inventario($_POST['inventario_id_Inventario']);
+    $mDto->setId_Movimiento($id_Movimiento);
+    $mDto->setCantidad_despues($cantidad_despues);
+    $mDto->setFecha_movimiento($fecha_movimiento);
+    $mDto->setFecha_modificacion($fecha_modificacion);
+    $mDto->setEstado_despues($estado_despues);
+    $mDto->setInventario_id_Inventario($inventario_id_Inventario);
 
     $mensaje = $mDao->registrarMovimiento($mDto);
-    echo $mensaje;
     if ($mensaje === 'Registrado Exitosamente') {
-        header("Location:../../PAGINA/registro.php?registro=exitoso");
-        exit;
+        echo json_encode(['success' => true]);
+        exit();
+        
     }
-
-
-}
-else if (isset($_POST['registrocrud'])){
+} else if (isset($listar) ||isset($_GET['si'])) {
+    $mDao = new MovimientoDao();
+    $mDto = new MovimientoDto();
+    $lista = $mDao->listarTodos();
+    $response = []; 
+    foreach ($lista as $movimiento) {
+        // Asegúrate de que cada  sea un array o un objeto
+        $response[] = [
+            $movimiento['id_Movimiento'],
+            $movimiento['cantidad_despues'],
+            $movimiento['fecha_movimiento'],
+            $movimiento['fecha_modificacion'],
+            $movimiento['estado_despues'],
+            $movimiento['inventario_id_Inventario'],
+    ];
+    }
+    echo json_encode($response);
+    exit();
+}else if (isset($_POST['registrocrud'])){
     $mDao = new MovimientoDao();
     $mDto = new MovimientoDto();
     $mDto->setId_Movimiento($_POST['id_Movimiento']);
@@ -39,9 +81,10 @@ else if (isset($_POST['registrocrud'])){
         exit;
     }
 }
-else if ($_GET['id_Movimient']!=null){
+else if ($_GET['id_Movimiento']!=null){
+    
     $mDao = new MovimientoDao();
-    $mensaje = $mDao->eliminarMovimiento($_GET['id_Movimient']);
+    $mensaje = $mDao->eliminarMovimiento($_GET['id_Movimiento']);
     header("Location:../tablas/movimiento/listarmovimiento.php?mensaje=".$mensaje);
     exit();
 }
---
+++ adax/src/components/CRUD/movimiento.jsx
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import DataTable from 'datatables.net-react';
+import DT from 'datatables.net-dt';
+import axios from 'axios';
+
+
+DataTable.use(DT);
+
+const Movimiento = () => {
+    const [movimientos, setMovimientos] = useState([]);
+
+    const Lista = async () => {
+        try {
+            const respuesta = await axios.post(
+                'http://localhost/adx/ADAX-Store-Manager/Crud/controlador/controlador.movimiento.php', 
+                { listar: true }
+            );
+            console.log(respuesta.data); // Verifica los datos aquí
+            if (respuesta.data) {
+                setMovimientos(respuesta.data);
+            } else {
+                console.log('Listado no exitoso:', respuesta.data);
+            }
+        } catch (err) {
+            console.error('Error al obtener los datos:', err);
+        }
+    };
+    
+
+    useEffect(() => {
+        Lista();
+    }, []);
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body sticky-top" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="../tablas.php">ADAX - CRUD</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#topnavbarNavDropdown"
+                        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item dropdown"><a className="nav-link dropdown-toggle" href="#top" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">Usuarios</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="../usuario/listarusuarios.php">lista</a></li>
+                                    <li><a className="dropdown-item" href="../usuario/registrar.php">registrar</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown"><a className="nav-link dropdown-toggle" href="#top" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">Tienda</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="../tienda/listartienda.php">lista</a></li>
+                                    <li><a className="dropdown-item" href="../tienda/registrar.php">registrar</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle " href="#top" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">Producto</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="../producto/listarproducto.php">lista</a></li>
+                                    <li><a className="dropdown-item" href="../producto/registrar.php">registrar</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#top" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">Factura</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="../factura/listarfactura.php">lista</a></li>
+                                    <li><a className="dropdown-item" href="../factura/registrar.php">registrar</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#top" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">Venta</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="../venta/listarventa.php">lista</a></li>
+                                    <li><a className="dropdown-item" href="../venta/registrar.php">registrar</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#top" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">Proveedor</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="../proveedor/listarproveedor.php">lista</a></li>
+                                    <li><a className="dropdown-item" href="../proveedor/registrar.php">registrar</a></li>
+                                </ul>
+                            </li>
+
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle active" href="#top" role="button" data-bs-toggle="dropdown" aria-expanded="false">Movimiento</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="../movimiento/listarmovimiento.php">lista</a></li>
+                                    <li><a className="dropdown-item" href="../movimiento/registrar.php">registrar</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <span className="navbar-text me-3 active">Usuario:
+                        </span>
+                        <a href="cerrarsesion.php" className="btn btn-outline-danger float-right end-0 me-0" type="submit">cerrar
+                            sesión</a>
+                    </div>
+                </div>
+            </nav>
+            <div style={{ width: '99.9%' }}>
+                <DataTable
+                    data={movimientos}
+                    slots={{
+                        6: (data, row) => (
+                            <form action="movimiento/actualizar.php" method="post">
+                                <input type="hidden" name="id_Movimiento" value={row.id_Movimiento} />
+                                <button type="submit" className="btn btn-warning">Modificar</button>
+                            </form>
+                        ),
+                        7: (data, row) => (
+                            <a
+                                className="btn btn-danger"
+                                href={`../../controlador/controlador.movimiento.php?id_Movimiento=${row.id_Movimiento}`}
+                            >
+                                Eliminar
+                            </a>
+                        ),
+                    }}
+                    id="usrtable"
+                    className="table table-container table-striped table-hover table-bordered table-responsive mt-4 table-sm"
+                >
+                    <thead className="table-dark light-header">
+                        <tr className="text-center">
+                            <th>id_Movimiento</th>
+                            <th>cantidad_despues</th>
+                            <th>fecha_movimiento</th>
+                            <th>fecha_modificacion</th>
+                            <th>estado_despues</th>
+                            <th>id_tienda</th>
+                            <th>Modificar</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </DataTable>
+            </div>
+        </div>
+    );
+};
+
+export default Movimiento;
