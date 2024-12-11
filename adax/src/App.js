9fe953bd0f74e05528b49570ca44af3587c89941
LEFINIT <yeyersyt@gmail.com>
2024-12-10 21:18:26 -0500

    cambios en el crud

--- adax/src/App.js
+++ adax/src/App.js
@@ -5,7 +5,7 @@ import Index from './components/index.jsx';
 import IniciarSesion from './components/iniciar_sesion.jsx';
 import Registro from './components/registro.jsx';
 import Restablecer from './components/restablecercontrasena.jsx';
-import Factura from './components/factura.jsx';
+import Factura from './components/factura.jsx'; // Adjusted to singular "factura.jsx"
 import Inicio from './components/inicio.jsx';
 import Pago from './components/generar_pago.jsx';
 import Ventas from './components/ventas.jsx';
@@ -18,6 +18,10 @@ import Usuarios from './components/CRUD/usuarios.jsx';
 import ActualizarUsuarios from './components/CRUD/actualizar/actualizarUsuario.jsx';
 import Tienda from './components/CRUD/tienda.jsx';
 import Producto from './components/CRUD/producto.jsx';
+import Movimiento from './components/CRUD/movimiento.jsx';
+import Facturass from './components/CRUD/facturass.jsx';
+import Proveedor  from './components/CRUD/proveedor.jsx';
+
 function App() {
   return (
     <ProveedorSesion>
@@ -28,18 +32,21 @@ function App() {
           <Route path="/registro" element={<Registro />} />
           <Route path="/restablecer_contrasena" element={<Restablecer />} />
           <Route path="/generar_pago" element={<Pago />} />
-          <Route path="/factura" element={<Factura />} />
+          <Route path="/factura" element={<Factura />} /> 
+          <Route path="/crud/facturass" element={<Facturass />} />
           <Route path="/ventas" element={<Ventas />} />
-          <Route path="/gestionar_productos" element={< GestionarProductos />} />
+          <Route path="/gestionar_productos" element={<GestionarProductos />} />
           <Route path="/gestionar_proveedores" element={<Gestionarproveedores />} />
           <Route path="/inicio" element={<Inicio />} />
           <Route path="/analisis" element={<Analisis />} />
+          <Route path="/crud/movimiento" element={<Movimiento />} />
           <Route path="/gestionar_ventas" element={<GestionarVentas />} />
           <Route path="/detalle_factura" element={<DetalleFactura />} />
           <Route path="/crud/usuarios" element={<Usuarios />} />
           <Route path="/crud/actualizar/actualizar_usuario" element={<ActualizarUsuarios />} />
           <Route path="/crud/tienda" element={<Tienda />} />
           <Route path="/crud/producto" element={<Producto />} />
+          
           <Route path="*" element={<Index />} />
         </Routes>
       </div>
