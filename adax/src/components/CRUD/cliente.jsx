2d30766d4479a8aa86a0920a0e4a66d3b853b6de
LEFINIT <yeyersyt@gmail.com>
2025-01-10 15:21:56 -0500

    Listar Inventario y Ventas hay 12 listas

--- adax/src/components/CRUD/cliente.jsx
+++ adax/src/components/CRUD/cliente.jsx
@@ -46,7 +46,13 @@ const Cliente = () => {
     }
     const handleInventario = () => {
         navigate("/crud/inventario")
-    } 
+    }
+    const handleMetodosDePago = () => {
+        navigate("/crud/metodosdepago")
+    }
+    const handleVentas = () => {
+        navigate("/crud/ventas")
+    }
     DataTable.use(DT);
     const [clientes, setClientes] = useState([]);
     // eslint-disable-next-line
@@ -166,7 +172,23 @@ const Cliente = () => {
                                     <li><button className="dropdown-item" onClick={handleRegistro}>registrar</button></li>
                                 </ul>
                             </li>
+
                             
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#top" role="button" data-bs-toggle="dropdown" aria-expanded="false">Metodos de Pago</a>
+                                <ul className="dropdown-menu">
+                                    <li><button className="dropdown-item" onClick={handleMetodosDePago}>lista</button></li>
+                                    <li><button className="dropdown-item" onClick={handleRegistro}>registrar</button></li>
+                                </ul>
+                            </li>
+
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#top" role="button" data-bs-toggle="dropdown" aria-expanded="false">Ventas</a>
+                                <ul className="dropdown-menu">
+                                    <li><button className="dropdown-item" onClick={handleVentas}>lista</button></li>
+                                    <li><button className="dropdown-item" onClick={handleRegistro}>registrar</button></li>
+                                </ul>
+                            </li>
                         </ul>
                         <span className="navbar-text me-3 active">Usuario: {usuario}
                         </span>
