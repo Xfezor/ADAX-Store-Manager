e1f0f5ee9e0a4b1bd27ddc2e677aee3836f0e23a
BrigittBarbosa <barbosabrigith@gmail.com>
2025-03-31 19:23:38 -0500

    app

--- Adax-Store-Manager-App-Movil/App.js
+++ Adax-Store-Manager-App-Movil/App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Pantallas
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Bienvenido a la pantalla de inicio!</Text>
+      <Button
+        title="Ir a la pantalla de Detalles"
+        onPress={() => navigation.navigate('Details')}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Pantalla de Detalles</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
