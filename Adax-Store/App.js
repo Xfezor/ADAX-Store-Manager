c26c66d1369c52a17b15d6102879ba3f2bb0eeab
Brigitt Barbosa <barbosabrigith@gmail.com>
2025-04-07 17:24:48 -0500

    Vistas terminadas en diseño

--- Adax-Store/App.js
+++ Adax-Store/App.js
@@ -7,6 +7,9 @@ import OlvidoContrasenaCodigo from "./views/olvidocontrasenacodigo";
 import OlvidoContrasenaCorreo from "./views/olvidocontrasenacorreo";
 import IniciarSesion from "./views/iniciarsesion";
 import RegistrarUsuario from "./views/RegistrarUsuario";
+import RegistrarUsuarios2 from "./views/RegistrarUsuarios2";
+import RegistrarTienda from "./views/RegistrarTienda";
+import RegistrarTiendas2 from "./views/RegistrarTiendas2";
 import OlvidoContrasena from "./views/contrasenaolvidar";
 import MenuPrincipal from "./views/menuPrincipal";
 import Actualizar from "./views/actualizar"; 
@@ -25,6 +28,9 @@ export default function App() {
     <Stack.Screen name="OlvidoContrasenaCorreo" component={OlvidoContrasenaCorreo} />
     <Stack.Screen name="OlvidoContrasena" component={OlvidoContrasena} />
     <Stack.Screen name="RegistrarUsuario" component={RegistrarUsuario} />
+    <Stack.Screen name="RegistrarUsuarios2" component={RegistrarUsuarios2} />
+    <Stack.Screen name="RegistrarTienda" component={RegistrarTienda} />
+    <Stack.Screen name="RegistrarTiendas2" component={RegistrarTiendas2} />
     <Stack.Screen name="MenuPrincipal" component={MenuPrincipal} />
     <Stack.Screen name="Actualizar" component={Actualizar} />
     </Stack.Navigator>
---
+++ Adax-Store/views/RegistrarTiendas2.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+const RegistrarTiendas2 = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./assets/logo.png')} style={styles.logo} />
+      <View style={styles.formContainer}>
+        <Text style={styles.title}>Registrarse</Text>
+
+        <View style={styles.form}>
+          <Text style={styles.label}>Correo Electrónico</Text>
+          <TextInput style={styles.input} placeholder="Ingresa su correo electrónico" placeholderTextColor="#AFAFAF" />
+
+          <Text style={styles.label}>Contraseña</Text>
+          <TextInput style={styles.input} placeholder="Ingresa su contraseña" placeholderTextColor="#AFAFAF" secureTextEntry={true} />
+
+          <Text style={styles.label}>Confirmar Contraseña</Text>
+          <TextInput style={styles.input} placeholder="Repite la contraseña" placeholderTextColor="#AFAFAF" secureTextEntry={true} />
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Registrarse</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FCEDC0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+    marginBottom: 20,
+  },
+  formContainer: {
+    backgroundColor: '#EBD8A0',
+    padding: 25,
+    borderRadius: 20,
+    width: '90%',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'Montserrat',
+    marginBottom: 10,
+    color: '#000',
+    textAlign: 'center',
+  },
+  form: {
+    width: '100%',
+  },
+  label: {
+    fontSize: 20,
+    fontWeight: 'Montserrat',
+    marginTop: 10,
+    color: '#000',
+  },
+  input: {
+    backgroundColor: 'white',
+    padding: 12,
+    borderRadius: 15,
+    marginTop: 5,
+    fontSize: 16,
+    color: '#333',
+    borderWidth: 1,
+    borderColor: '#D3A86A',
+  },
+  button: {
+    backgroundColor: '#F85F6A',
+    padding: 8,
+    borderRadius: 15,
+    alignItems: 'center',
+    marginTop: 30,
+    width: '50%',
+    alignSelf: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'Montserrat',
+  },
+});
+
+export default RegistrarTiendas2;
