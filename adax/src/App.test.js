a69b1dba64077ef852fb56ab039aef06de29ebc8
Xfezor <damono600d@gmail.com>
2025-05-20 13:21:40 -0500

    Jest instalado en el proyecto con babel y otras dependencias para el uso de jest y otros componentes sin errores, tambien esta conigurado para common js

--- adax/src/App.test.js
+++ adax/src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('test de prueba: renderiza el componente App sin errores', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  // No se realiza ninguna búsqueda ni aserción
 });
--- adax/src/components/index.jsx
+++ adax/src/components/index.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../styles/style_index.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
