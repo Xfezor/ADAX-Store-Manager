8bd1c74246278a68568d1fd1db99da2944dc46d3
Xfezor <damono600d@gmail.com>
2025-06-04 20:01:44 -0500

    otro intento de arreglar la base de datos

--- ADAX-STORE-DB_V8.sql
+++ ADAX-STORE-DB_V8.sql
@@ -748,6 +748,27 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
 DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `desencriptarClaveTienda`(`docu` INT) RETURNS varchar(50) CHARSET utf8mb4 COLLATE utf8mb4_general_ci
+BEGIN
+declare contrasenaDesencriptada varchar(45);
+select aes_decrypt(contrasena, "adaxdecripter2024") into contrasenaDesencriptada from tienda where documento = docu;
+RETURN contrasenaDesencriptada;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `obtener_historial_movimientos` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` FUNCTION `obtener_historial_movimientos`(`id_Venta` INT(11)) RETURNS decimal(10,2)
 BEGIN
     DECLARE costo_total DECIMAL(10,2);
