febe3fdae1ff061fc6fc16522c9a94ab050c1a12
Xfezor <damono600d@gmail.com>
2025-06-04 22:42:06 -0500

    vuelta a configurar la ip por decima vez

--- adax/src/utils/ipconfig.js
+++ adax/src/utils/ipconfig.js
@@ -1,4 +1,4 @@
 // En este archivo se define la IP y el puerto del servidor de la API
-export const ip = "localhost"; 
+export const ip = "adaxstore.xyz"; 
 export const port = "80";
 export const protocol = "https"; // Cambia a "http" si es necesario
