acc4eef3305e04e8e248126fdd931ceca8d279b1
Xfezor <damono600d@gmail.com>
2025-06-04 22:37:54 -0500

    cambio de la ip

--- adax/src/utils/ipconfig.js
+++ adax/src/utils/ipconfig.js
@@ -1,4 +1,4 @@
 // En este archivo se define la IP y el puerto del servidor de la API
 export const ip = "localhost"; 
 export const port = "80";
-export const protocol = "${protocol}s"; // Cambia a "${protocol}s" si es necesario
+export const protocol = "https"; // Cambia a "http" si es necesario
